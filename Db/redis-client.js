7916b15e7834c285b24b5e9201bf9563b4e14419
shouryade <shourya.de12@gmail.com>
2024-04-04 04:31:17 +0530

    dotenv redis
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- Db/redis-client.js
+++ Db/redis-client.js
@@ -1,7 +1,9 @@
 import { createClient } from "redis";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = createClient({
-  url: "redis://default:default@localhost:6379",
+  url: `redis://default:default@${process.env.REDIS_URL}:${process.env.REDIS_PORT}`,
 });
 
 client
