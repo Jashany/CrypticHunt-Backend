be13e48dc8316d4d99ceb9ad8b4757bfbdf2dba2
shouryade <shourya.de12@gmail.com>
2024-04-04 02:50:31 +0530

    mongodb
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- Db/index.js
+++ Db/index.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import Team from "../Models/Team.model.js";
 
 const ConnectDB = async () => {
   try {
     const connection = await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log(`MongoDB connected : ${connection.connection.host}`);
+    // await Team.updateMany({}, { $set: { solvedQuestions: [] } });
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
     process.exit(1);
